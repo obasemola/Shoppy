d28b41d510f3f5ea5c25e6678cb4c69aadae692e
Ayodele obasemola <obasemolaayodele@gmail.com>
2020-07-03 16:30:45 +0300

    completed signup, signout, and signin flow

--- index.js
+++ index.js
@@ -82,8 +82,25 @@ app.get('/signin', (req, res) => {
     </form>
   </div>
     `
-  )
-})
+  );
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password} = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if(!user) {
+    return res.send('Email not found');
+  }
+
+  if (user.password !== password) {
+    return res.send('invalid password');
+  }
+  
+  req.session.userId = user.id;
+  res.send('You are signed in');
+});
 
 
 app.listen(3000, () => {
